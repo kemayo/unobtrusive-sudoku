b62ebb06c905d0fbe52a12a092a83bad93507032
David Lynch <kemayo@gmail.com>
2011-05-26 00:27:08 -0500

    Fix. Change to jQuery, incidentally.

--- sudoku.js
+++ sudoku.js
@@ -1,56 +1,43 @@
-base2.DOM.bind(document);
+$(function($) {
 
-makeSudoku = function(id, puzzle) {
+var makeSudoku, makeSudokuRow, makeSudokuCell, makeNoteBox, showNotes,
+	isValidSudokuChar, getkey, checkSetAndMark, clearProblem, sudokuCheck,
+	getRowFor, getColumnFor, getSquareFor, getSquareBase, getCell,
+	populateSudoku;
+
+makeSudoku = window.makeSudoku = function(id, puzzle) {
 	// Creates a sudoku puzzle within the element with the given id, replacing any existing content.  Should probably be a div, or suchlike.
-	var base = document.getElementById(id);
-	var ci;
-	for (ci=0; ci<base.childNodes.length; ci++) {
-		base.removeChild(base.childNodes[ci]);
-	}
+	var base = $('#'+id).empty()
+		,t = $('<table class="sudoku"></table>').appendTo(base)
+		;
 	
-	var t = document.createElement('table');
-	base2.DOM.bind(t);
-	t.addClass('sudoku');
-	var tbody = document.createElement('tbody');
-	t.appendChild(tbody);
-	var i = 1;
-	for (i=1; i < 10; i++) {
-		makeSudokuRow(id, tbody, i)
+	for (var i = 1; i < 10; i++) {
+		t.append(makeSudokuRow(id, i));
 	}
-	base.appendChild(t);
 	
 	makeNoteBox(id);
 	
-	var checkButton = document.createElement('button');
-	checkButton.appendChild(document.createTextNode("Check for errors"));
-	checkButton.onclick = function() { sudokuCheck(id); return false; }
-	base.appendChild(checkButton);
+	$('<button>Check for errors</button>').click(function(e) {
+		e.preventDefault();
+		sudokuCheck(id);
+	}).appendTo(base);
 }
-makeSudokuRow = function(id, table, y) {
-	var row = document.createElement('tr');
-	base2.DOM.bind(row);
-	row.addClass('sudokurow'+y);
-	var ii = 1;
-	for (ii=1; ii < 10; ii++) {
-		makeSudokuCell(id, row, ii, y);
+
+makeSudokuRow = function(id, y) {
+	var row = $('<tr class="sudokurow' + y + '"></tr>');
+	for (var ii=1; ii < 10; ii++) {
+		row.append(makeSudokuCell(id, ii, y));
 	}
-	table.appendChild(row);
+	return row;
 }
-makeSudokuCell = function(id, row, x, y) {
-	var cell = document.createElement('td');
-	base2.DOM.bind(cell);
-	cell.addClass('sudokucolumn'+x);
-	var input = document.createElement('input');
-	base2.DOM.bind(input);
-	input.addClass('sudokucell' + y + x);
-	input.onkeypress = isValidSudokuChar;
-	input.onclick = function(event) { showNotes(id,x,y); }
-	input.maxLength = 1;
-	input.value = '';
-	cell.appendChild(input);
-	row.appendChild(cell);
+makeSudokuCell = function(id, x, y) {
+	return $('<td class="sudokucolumn' + x + ' sudokusquare' + getSquareBase(y) + getSquareBase(x) + '"></td>').append(
+		$('<input class="sudokucell' + y + x + '" maxlength="1" value="">')
+			.keypress(isValidSudokuChar)
+			.click(function(e) { showNotes(id, x, y); })
+	);
 }
-var notes = new Object;
+var notes = {};
 makeNoteBox = function(id) {
 	var box = document.createElement('div');
 	notes[id] = box;
@@ -58,106 +45,86 @@ makeNoteBox = function(id) {
 showNotes = function(id, x, y) {
 	
 }
-function getkey(e)
-{
-	if (window.event) {
-		return window.event.keyCode;
-	} else if (e) {
-		return e.which;
-	} else {
-		return null;
-	}
-}
+
 isValidSudokuChar = function(e) {
-	var key, keychar;
-	key = getkey(e);
-	if (key == null) { return true; }
+	var key = e.which
+		,pattern = "123456789".toLowerCase()
+		,keychar
+		;
+	if (key == null) {
+		return;
+	}	
 	keychar = String.fromCharCode(key).toLowerCase();
-	var pattern="123456789";
-	pattern = pattern.toLowerCase();
 	// check pattern
-	if (pattern.indexOf(keychar) != -1)
-		return true;
+	if (pattern.indexOf(keychar) != -1) {
+		return;
+	}
 	// control keys
-	if ( key==null || key==0 || key==8 || key==9 || key==13 || key==27 )
-		return true;
-	return false;
+	if ( key==null || key==0 || key==8 || key==9 || key==13 || key==27 ) {
+		return;
+	}
+	
+	e.preventDefault();
 }
-clearProblem = function(element) {
-	element.removeClass('problem');
-}
+
 sudokuCheck = function(id) {
-	document.matchAll('#'+id+' .problem').forEach(clearProblem);
-	var i;
-	for(i=1; i<10; i++) {
+	$('#'+id+' .problem').removeClass('problem');
+	
+	for(var i = 1; i < 10; i++) {
 		checkSetAndMark(getRowFor(id, 1, i));
 		checkSetAndMark(getColumnFor(id, i, 1));
-		checkSetAndMark(getSquareFor(id, ((i-1)%3)*3, (Math.floor((i-1)/3) % 3) + 1));
+		checkSetAndMark(getSquareFor(id
+									 ,1 + ((i - 1) % 3) * 3
+									 ,getSquareBase(i)
+						));
 	}
 }
 checkSetAndMark = function(a) {
-	var i;
-	var alreadySeen = new Array();
-	for(i=1; i < a.length; i++) {
-		var v = a[i].value;
-		if (v != '') {
-			if(alreadySeen[v]) {
-				a[alreadySeen[v]].parentNode.addClass('problem');
-				a[i].parentNode.addClass('problem');
-			}
-			alreadySeen[v] = i;
+	var alreadySeen = {};
+	a.each(function(i, cell) {
+		cell = $(cell);
+		var val = cell.val();
+		if(val == "") { return; }
+		if(alreadySeen[val]) {
+			a.filter('[value="' + val + '"]').closest('td').addClass('problem');
 		}
-	}
+		alreadySeen[val] = true;
+	});
 }
-getRowFor = function(id,x,y) {
-	var i = 1;
-	var row = new Array();
-	for (i=1; i < 10; i++) {
-		row[i] = getCell(id,i,y);
-	}
-	return row;
+getRowFor = function(id, x, y) {
+	return $('#' + id + ' .sudokurow' + y + ' input');
 }
 getColumnFor = function(id,x,y) {
-	var i = 1;
-	var column = new Array();
-	for (i=1; i < 10; i++) {
-		column[i] = getCell(id,x,i);
-	}
-	return column;
+	return $('#' + id + ' .sudokucolumn' + x + ' input');
 }
-getSquareFor = function(id,x,y) {
-	var i;
-	var square = new Array();
-	for (i=0; i < 9; i++) {
-		square[i+1] = getCell(id, getSquareBase(x)+(i%3), getSquareBase(y)+(Math.floor(i/3)%3))
-	}
-	return square;
+getSquareFor = function(id, x, y) {
+	return $('#' + id + ' .sudokusquare' + getSquareBase(y) + getSquareBase(x) + ' input');
 }
-getCell = function(id,x,y) {
-	return document.matchSingle('#'+id+' input.sudokucell'+y+x);
+getCell = function(id, x, y) {
+	return $('#' + id + ' input.sudokucell' + y + x); 
 }
 getSquareBase = function(i) {
 	if (i < 4) { return 1; }
 	if (i < 7) { return 4; }
 	if (i < 10) { return 7; }
 }
-populateSudoku = function(id, puzzle) {
+populateSudoku = window.populateSudoku = function(id, puzzle) {
 	var puzzlearray = puzzle.split('');
-	var ii;
-	for(ii=0; ii<puzzlearray.length; ii++) {
-		var x = (ii % 9) + 1;
-		var y = (Math.floor(ii/9) % 9) + 1;
-		var cell = getCell(id, x, y);
-		var v;
+	for(var ii = 0; ii < puzzlearray.length; ii++) {
+		var x = (ii % 9) + 1
+			,y = (Math.floor(ii/9) % 9) + 1
+			,cell = getCell(id, x, y)
+			,v
+			;
 		if (puzzlearray[ii] == 0) {
 			v = '';
-			cell.disabled = false;
-			cell.removeClass('provided');
+			cell.attr('disabled', false).removeClass('provided');
 		} else {
 			v = puzzlearray[ii];
-			cell.disabled = true;
-			cell.addClass('provided');
+			cell.attr('disabled', true).addClass('provided');
 		}
-		cell.value = v;
+		cell.val(v);
 	}
 }
+
+});
